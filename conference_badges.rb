9b42b798adc638ebabe639fc238bf1c347dc37c4
Kevin Chi <kevinchi118@gmail.com>
2018-06-12 22:08:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+    puts "Hello, my name is #{name}"
+end
+
+def batch_badge_creater(array)
+    array.each do |name|
+        badge_maker(name)
+    end
+end
